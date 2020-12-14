e3c4c0b15e3ee79c873793faee3f417d963d3bdc
Colwyn Gulliford <cg248@cornell.edu>
2020-12-14 18:43:34 -0500

    Update element.py

--- gpt/element.py
+++ gpt/element.py
@@ -687,7 +687,7 @@ class Lattice():
             ax.set_aspect('equal')
 
         if(not box_on):
-            ax.set_visible(False)
+            ax.box(on=False)
 
         if(not labels_on):
             ax.get_xaxis().set_ticks([])
