5a65e377bfb72ac1eb636292531840d949114008
Colwyn Gulliford <cg248@cornell.edu>
2021-10-17 12:46:44 -0400

    Versioneer support

--- gpt/__init__.py
+++ gpt/__init__.py
@@ -2,3 +2,5 @@ from .gpt import GPT, run_gpt
 
 from .evaluate import evaluate_gpt
 from .gpt_distgen import run_gpt_with_distgen, evaluate_gpt_with_distgen
+from . import _version
+__version__ = _version.get_versions()['version']
--- setup.py
+++ setup.py
@@ -1,17 +1,19 @@
 
+import versioneer
 from setuptools import setup, find_packages
 from os import path, environ
+
 
 cur_dir = path.abspath(path.dirname(__file__))
 
 with open(path.join(cur_dir, 'requirements.txt'), 'r') as f:
     requirements = f.read().split()
 
-
-
 setup(
+
     name='lume-gpt',
-    version = '0.5.0',
+    version = versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(), 
     packages=find_packages(), 
     package_dir={'lume-gpt':'lume-gpt'},
     url='https://github.com/ColwynGulliford/lume-gpt',
@@ -19,5 +21,5 @@ setup(
     long_description_content_type='text/markdown',
     install_requires=requirements,
     include_package_data=True,
-    python_requires='>=3.6'
+    python_requires='>=3.7'
 )
